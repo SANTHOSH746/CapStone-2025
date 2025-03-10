a74a5ba381efc0357402c3205845a4f125cb606e
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-03-10 11:46:46 +0530

    fcfcf

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./components/JSX/Login";
+
+const App = () => {
+    return (
+        <>
+            
+            <Login />
+        </>
+    );
+};
+
+export default App;
